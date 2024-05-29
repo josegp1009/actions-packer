09d68ed3fbd1d2548321df3de9a183428aa767c5
fuji komalan <fujikomalan@gmail.com>
2024-05-29 15:12:10 +0530

    version1

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+echo "ClientAliveInterval 60" >> /etc/ssh/sshd_config
+systemctl restart sshd.service
+
+yum install httpd php -y
+
+mkdir -p /var/www/html/billing/
+
+echo "testing" > /var/www/html/billing/health.html
+
+cat <<EOF > /var/www/html/billing/index.php
+<?php
+\$output = shell_exec('echo $HOSTNAME');
+echo "<h1><center><pre>\$output</pre></center></h1>";
+echo "<h1><center>Demo Website Version1</center></h1>";
+?>
+EOF
+
+
+systemctl restart httpd.service php-fpm.service
+systemctl enable httpd.service php-fpm.service
---
+++ variables.pkr.hcl
@@ -0,0 +1,28 @@
+
+variable "project_name" {
+  type = string
+  default = "zomato"
+}
+
+variable "project_env" {
+  type = string
+  default = "prod"
+}
+
+
+variable "ami" {
+  type = string
+  default = "ami-013168dc3850ef002"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+locals {
+  
+  timestamp = "${formatdate("DD-MM-YYYY-hh-mm", timestamp())}"
+  image_name = "${var.project_name}-${var.project_env}-${local.timestamp}"
+}
+
